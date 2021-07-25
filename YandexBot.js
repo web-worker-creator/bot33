2c385f9b12f3386dcb40d74cd047cb8e9c519f90
web-worker-creator <85514863+web-worker-creator@users.noreply.github.com>
2021-07-25 15:33:14 +0300

    Update YandexBot.js

--- YandexBot.js
+++ YandexBot.js
@@ -5,28 +5,38 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://gufo.me/dict/bse/%D0%93%D0%BE%D0%B1%D0%BE%D0%B9/*
 // @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
-let worlds = ["Гобой", "Флейта","Как звучит кларнет","Валторна","Фагот"];
+let sites = {
+    "%D0%93%D0%BE%D0%B1%D0%BE%D0%B9":["Гобой", "Флейта","Как звучит кларнет","Валторна","Фагот"],
+    "crushdrummers.ru":["барабанное шоу", "Заказать барабанщиков", "Барабанщики на праздник Москва"]
+}
+let site = Object.keys(sites)[getIntRandom(0, Object.keys(sites).length)];
+let worlds = sites[site];
 let yandexInput = document.getElementsByName("text")[0];
-yandexInput.value= worlds[getIntRandom(0, worlds.length)];
+let word = worlds[getIntRandom(0, worlds.length)]
 let buttn = document.getElementsByClassName("button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited")[0];
-setTimeout(function(){
-    buttn.click();
-},1700);
-let linux = document.getElementsByClassName("Link Link_theme_normal OrganicTitle-Link OrganicTitle-Link_wrap Typo Typo_text_l Typo_line_m organic__url link i-bem link_js_inited")[0];
-if(linux != undefined){
-    setTimeout(function(){
-        linux.click();
-    },1500);
-}else{
+if(buttn != undefined){
+    document.cookie = "site="+site;
+    let i = 0;
+    let timerId = setInterval(()=>{
+        yandexInput.value += word[i++];
+        if(i==word.length){
+        clearInterval(timerId);
+            buttn.click();
+        }
+    }, 1000);
+} else if(location.hostname == "https://yandex.ru/"){
+    let site = getCookie('site');
     let links= document.links;
-    let pgnext = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next i-bem link_js_inited");
+    let pgnext = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next i-bem");
     let goToTheNextPage = true;
+    let currentPage = +document.querySelector("pager__item pager__item_current_yes pager__item_kind_page").innerText;
     for(let i=0; i<links.length; i++){
         let link = links[i];
-        if(link.href.indexOf("https://wikipedia.pp.ru/%D0%92%D0%B0%D0%BB%D1%82%D0%BE%D1%80%D0%BD%D0%B0") != -1){
+        if(link.href.indexOf(site) != -1){
             setTimeout(function(){
                 link.click();
             },2000);
@@ -34,8 +44,26 @@ if(linux != undefined){
             break;
         }
     }
-   if(goToTheNextPage) setTimeout(function(){ pgnext.click();}, 1800);
+  if(goToTheNextPage && currentPage<7) setTimeout(function(){ pgnext.click();}, 1800);
+   else if(goToTheNextPage) location.href = "https://yandex.ru/";
+}else{
+    let links = document.links;
+    let index = getIntRandom(0,links.length)
+    setTimeout(()=>{
+        if (links[index].href.indexOf(location.pathname) != -1 )
+            links[index].click();
+        else location.href = "https://yandex.ru/";
+    },2000);
+
+    console.log("Мы больше не на яндексе");
 }
 function getIntRandom(min,max){
     return Math.floor(Math.random()*(max-min)-min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
